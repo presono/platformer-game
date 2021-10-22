9941c1873f4704e69e9b2097820d49d0146744ec
presono <75654773+presono@users.noreply.github.com>
2021-10-22 10:49:44 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Stick":return img`
+a a a a a a a a b b a a a a b b 
+b a b b b b a b b b b b b b b b 
+b b b a a b b b a a a a a a b b 
+a b b 9 . . . . . . . 9 9 b b a 
+b a b 9 . . 9 9 9 9 9 9 . b b b 
+a b b 9 9 f f f f f f . 9 a a b 
+a b a 9 9 f f f f f f 9 9 a b b 
+. . . 9 . f f 2 2 f f 9 9 . . . 
+. . . 9 9 f f 2 2 f f . 9 . . . 
+b a a . 9 f f f f f f . 9 a a a 
+a b a . 9 f f f f f f 9 9 b b a 
+a b b 9 9 . 9 9 9 9 9 9 . a b a 
+b b a 9 9 9 9 . . 9 9 . . a a b 
+b a a a b a a a a a a a b b b b 
+b b a a a b b b a a b b b b a a 
+a b b b b a a b b b a a b a a a 
+`;
+            case "image2":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 6 6 . 6 6 6 . . . . . 
+. . . 3 6 6 6 6 6 6 3 6 6 6 . . 
+. . 6 6 6 6 6 6 6 6 6 6 f 6 6 . 
+. 6 6 6 f 3 6 f 3 6 f f 6 3 6 . 
+. 6 6 f f 6 6 6 f 6 f 6 6 6 . . 
+. 6 6 3 f f 6 6 f f 3 6 . 6 . . 
+. . 6 6 6 f f 6 f 6 6 . . . . . 
+. . . 6 6 6 f f f . 6 . . . . . 
+. . . . 6 . . f 6 . 6 . . . . . 
+. . . . 6 . . f . . . . . . . . 
+. . . . . . . f 6 . . . . . . . 
+. . . . . . . 6 f . . . . . . . 
+. . . . . . . 6 f . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . . f . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
